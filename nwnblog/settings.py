a91f7b6a583d7d8a0aac565cc1b8e092720b1613
Roshan Yadav <er.roshan.y@gmail.com>
2021-06-27 00:11:44 +0530

    third commit

--- nwnblog/settings.py
+++ nwnblog/settings.py
@@ -21,7 +21,7 @@ STATIC_DIR = Path(__file__).joinpath(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.envron['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
