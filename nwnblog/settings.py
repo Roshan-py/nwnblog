464a66bf4354d45bf45e8c9666bac2699806b82e
Roshan Yadav <er.roshan.y@gmail.com>
2021-06-27 20:25:25 +0530

    try2

--- nwnblog/settings.py
+++ nwnblog/settings.py
@@ -21,11 +21,11 @@ STATIC_DIR = Path(__file__).joinpath(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = ['django-insecure-6iw95ss)t!w=@@@fjfjh$(_qx_ozp+joky+4=i!=kewcbm6c6l']
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
 ALLOWED_HOSTS = ['nwnblog.herokuapp.com','*']
 
