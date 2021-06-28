b8d6c29b7f8e8ac1c01f1bfdde9d45eef53316cb
Roshan Yadav <er.roshan.y@gmail.com>
2021-06-28 11:08:49 +0530

    final v

--- nwnblog/settings.py
+++ nwnblog/settings.py
@@ -16,20 +16,18 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = Path(__file__).joinpath(BASE_DIR,'static')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","app.settings")
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = ['django-insecure-6iw95ss)t!w=@@@fjfjh$(_qx_ozp+joky+4=i!=kewcbm6c6l']
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
-ALLOWED_HOSTS = ['nwnblog.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['nwnblog.herokuapp.com','*']
 
 
 # Application definition
